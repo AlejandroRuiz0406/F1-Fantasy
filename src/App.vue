1111a592a56f22bf0d257e0c2c22fbf4bf0d5341
Alejandro Ruiz <loloresto04@gmail.com>
2025-06-28 11:57:04 +0200

    Primer commit 3

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
+<template>
+  <div>
+    <h1>¡Hola! Esta es la página principal</h1>
+    <button @click="fetchMessage">Obtener mensaje del backend</button>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
 <script setup>
 import { ref } from 'vue'
-
 const message = ref('')
 
 const fetchMessage = async () => {
-  const res = await fetch('/api/hello')  // ya no necesita localhost:3000
+  const res = await fetch('/api/hello')
   const data = await res.json()
   message.value = data.message
 }
