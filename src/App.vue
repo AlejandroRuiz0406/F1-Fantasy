6c992124a3ee033667a78300520f233ab9eb23b9
Alejandro Ruiz <loloresto04@gmail.com>
2025-06-28 11:53:57 +0200

    Primer commit 2

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('')
+
+const fetchMessage = async () => {
+  const res = await fetch('/api/hello')  // ya no necesita localhost:3000
+  const data = await res.json()
+  message.value = data.message
+}
+</script>
